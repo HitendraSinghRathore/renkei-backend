b2e2c9ac44e4f22ca6e3307479d88b27ddaf8048
HitendraSinghRathore <hitendersingh2@gmail.com>
2025-01-09 22:58:07 +0530

    basic node setup done

---
+++ src/config/index.js
@@ -0,0 +1,28 @@
+'use strict'
+const convict = require('convict');
+const path = require('path');
+const dotenv = require('dotenv');
+const env = process.env.NODE_ENV || 'development';
+if (env === 'development') {
+    // only load config when in local env
+    dotenv.config({ path: path.resolve(__dirname, '../../.env') });
+}
+
+const config = convict({
+    env: {
+        doc: 'The application environment.',
+        format: ['production', 'development', 'test'],
+        default: 'development',
+        env: 'NODE_ENV'
+    },
+    port: {
+        doc: 'The port to bind.',
+        format: 'port',
+        default: 3000,
+        env: 'PORT'
+    }
+});
+
+config.validate({ allowed: 'strict' });
+
+module.exports = config;
---
+++ src/server.js
@@ -0,0 +1,22 @@
+'use strict'
+const express = require('express');
+const config = require('./config');
+
+const app = express();
+
+const PORT = config.get('port');
+
+app.use(express.json());
+
+
+app.get('/healthz', function healthRouter(_,res) {
+    res.status(200).json({ status: 'OK'})
+});
+
+app.get('/',function defaultRouter(_,res)  {
+    res.send('Hello from the other side!!!')
+});
+
+app.listen(PORT, function serverSetup() {
+    console.log(`....Server started on PORT: ${PORT}......`)
+});
