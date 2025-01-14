a50066de0f349360eefb4b4cb02a669b6bf0c8b5
HitendraSinghRathore <hitendersingh2@gmail.com>
2025-01-14 11:39:06 +0530

    adding user model

--- src/app.js
+++ src/app.js
@@ -2,11 +2,20 @@
 const express = require('express');
 const corsMiddleware = require('./middlewares/cors');
 const mongoose = require('mongoose');
+const helmet = require('helmet');
+const cookieParser = require('cookie-parser');  
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
+
+app.use(helmet());
 app.use(corsMiddleware);
 
-app.use(express.json());
+
+
 
 
 app.get('/healthz', function healthRouter (_, res) {
