da442487959e9219d0fde2f1ed8cd4e58a668faa
HitendraSinghRathore <hitendersingh2@gmail.com>
2025-02-01 23:24:38 +0530

    update error code

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -10,15 +10,16 @@ async function authMiddleware(req, res, next) {
   const authHeader = req.headers.authorization;
   const accessToken = authHeader && authHeader.split(' ')[1];
   const refreshToken = req.cookies.refreshToken; 
-  console.log('---UNSAFE----');
-  console.log(req.cookies);
-  console.log('---UNSAFE----');
-  console.log(authHeader);
   if (!accessToken && !refreshToken) {
     // if no token provided, return 401
     return res.status(401).json({ msg: 'No token provided' , redirect: true });
   }
   try {
+    if(!accessToken) {
+      const error = new Error('Access token missing');
+      error.name = 'TokenExpiredError';
+      throw error;
+    }
     const decoded = jwt.verify(accessToken, config.get('jwtAuthSecret'));
     req.user = decoded; 
     if (!refreshToken) {
