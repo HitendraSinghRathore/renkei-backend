35d5a0f57198a123cfbfa8cfa301ea837611458a
HitendraSinghRathore <hitendersingh2@gmail.com>
2025-02-08 00:51:34 +0530

    update rules

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -43,7 +43,7 @@ async function authMiddleware(req, res, next) {
         const decodedUser = jwt.verify(refreshToken, config.get('jwtRefreshSecret'));
         console.log('Refresh token verified with user id: %o', decodedUser);
         const updatedUser = await User.findOneAndUpdate(
-          { _id: decodedUser.id, 'refreshToken.token': refreshToken },
+          { _id: decodedUser.id},
           { $set: { 'refreshToken.token': generateRefreshToken(decodedUser), 'refreshToken.expiresAt': new Date(Date.now() + 6 * 60 * 60 * 1000) } },
           { new: true } 
         );
